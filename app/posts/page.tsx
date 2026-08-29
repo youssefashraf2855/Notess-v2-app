c8db9f70d9fa57c923ddf384cbaca3fcee92f563
Your Name <youssefashraf2855@gmail.com>
2026-08-29 18:40:55 +0300

    Post v1.3 every post have its link when you click on it

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -8,8 +8,9 @@ export default async function Posts() {
       <h1>Posts</h1>
       {posts.map((post) => (
         <div key={post.id}>
-            <link rel="stylesheet" href={`/posts/${post.id}`} />
-          <h2>{post.title}</h2>
+            <a href={`/posts/${post.id}`}>
+          <h2 >{post.title}</h2>
+          </a>
           <p>{post.content}</p>
         </div>
       ))}
