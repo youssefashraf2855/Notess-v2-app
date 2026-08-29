57ed17249f3c1ef7b02a8fdd7f7a49d09b9aba4e
Your Name <youssefashraf2855@gmail.com>
2026-08-29 15:48:45 +0300

    Post

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,18 @@
-export default async function posts() {
-    return(
-        <div>Posts</div>
-    )
+import prisma from "@/lib/db";
+
+export default async function Posts() {
+  const posts = await prisma.note.findMany();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
