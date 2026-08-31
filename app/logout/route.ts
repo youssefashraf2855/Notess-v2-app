d0d4a634009467b2117b0bf3a2e9daf98600ee4e
Your Name <youssefashraf2855@gmail.com>
2026-08-31 21:54:26 +0300

    add auth middleware v1.1

--- app/logout/route.ts
+++ app/logout/route.ts
@@ -5,7 +5,5 @@ import { useRouter } from 'next/navigation';
 export async function POST() {
   const cookieStore = await cookies();
   cookieStore.delete('user_session');
-    const router = useRouter();
-    router.push("/sign-in")
   return NextResponse.json({ message: 'Logged out successfully' }, { status: 200 });
 }
