f9c888fe03090268bd26313eaad2a3f65e942cd7
Your Name <youssefashraf2855@gmail.com>
2026-08-31 22:27:17 +0300

    add auth middleware v2

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -24,7 +24,8 @@ export default function SignInPage() {
       setMessage(data.message); // Show error from API
     } else {
       setMessage("Signed in successfully!");
-      router.push("/posts"); // Redirect after success
+      router.push("/posts"); 
+      window.location.href = "/";// Redirect after success
     }
   };
 
